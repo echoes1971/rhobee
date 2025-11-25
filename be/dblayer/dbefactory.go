37c9fcd65fc74d3f4c6edd41c3f39f5a9591b753
Roberto@Zephyrus <roberto.roccoangeloni@gmail.com>
2025-11-25 18:28:28 +0100

    TableChildren: register which children can have a table

--- be/dblayer/dbefactory.go
+++ be/dblayer/dbefactory.go
@@ -1,11 +1,16 @@
 package dblayer
 
-import "log"
+import (
+	"log"
+	"slices"
+)
 
 type DBEFactory struct {
 	verbose        bool
 	classname2type map[string]DBEntityInterface
 	tablename2type map[string]DBEntityInterface
+
+	TableChildren map[string][]string
 }
 
 func NewDBEFactory(verbose bool) *DBEFactory {
@@ -24,6 +29,29 @@ func (dbef *DBEFactory) Register(dbe DBEntityInterface) {
 	}
 	dbef.classname2type[dbe.GetTypeName()] = dbe
 	dbef.tablename2type[dbe.GetTableName()] = dbe
+
+	if dbef.TableChildren == nil {
+		dbef.TableChildren = make(map[string][]string)
+	}
+	if !dbe.IsDBObject() {
+		return
+	}
+	for _, fk := range dbe.GetForeignKeys() {
+		parentTable := fk.RefTable
+		parentInstance := dbef.GetInstanceByTableName(parentTable)
+		if parentInstance == nil || !parentInstance.IsDBObject() {
+			continue
+		}
+		if _, exists := dbef.TableChildren[parentTable]; !exists {
+			dbef.TableChildren[parentTable] = make([]string, 0)
+		}
+		childTableName := dbe.GetTableName()
+		// Use slices.Contains to check if childTableName exists
+		if slices.Contains(dbef.TableChildren[parentTable], childTableName) {
+			continue
+		}
+		dbef.TableChildren[parentTable] = append(dbef.TableChildren[parentTable], childTableName)
+	}
 }
 
 func (dbef *DBEFactory) GetAllClassNames() []string {
