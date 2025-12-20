200f3ab3c55913e92a71da9c88ac00b99785a26a
Roberto@Zephyrus <roberto.roccoangeloni@gmail.com>
2025-12-20 12:50:21 +0100

    refactored

--- fe/src/DBPage.js
+++ fe/src/DBPage.js
@@ -1,4 +1,4 @@
-import React, { use } from 'react';
+import React, { use, useContext } from 'react';
 import { useState, useEffect, useRef } from 'react';
 import { ButtonGroup, Form, Spinner, Button, Overlay, Popover } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
@@ -16,6 +16,8 @@ import {
     classname2bootstrapIcon,
 } from './sitenavigation_utils';
 import axiosInstance from './axios';
+import { ObjectSearch } from './DBObject';
+import { ThemeContext } from './ThemeContext';
 
 // Configure Quill to preserve data-dbfile-id attribute
 
@@ -669,4 +671,30 @@ export function PageEdit({ data, onSave, onCancel, onDelete, saving, error, dark
             />
         </Form>
     );
+}
+export function Pages() {
+  const { t } = useTranslation();
+  const { dark, themeClass } = useContext(ThemeContext);
+  // const [query, setQuery] = useState("");
+  // const [editingFolder, setEditingFolder] = useState(null); // folder in editing
+  const searchClassname = "DBPage";
+
+  const searchColumns = [
+    { name: t("dbobjects.name") || "Name", attribute: "name", type: "string" },
+    // { name: t("dbobjects.description") || "Description", attribute: "description", type: "string" },
+    { name: t("dbobjects.parent") || "Parent", attribute: "father_id", type: "objectLink" },
+    { name: t("dbobjects.language") || "Language", attribute: "language", type: "languageSelector" },
+  ];
+
+  const resultsColumns = [
+    // { name: t("dbobjects.created") || "Created", attribute: "creator", type: "userLink", hideOnSmall: true },
+    // { name: t("dbobjects.group") || "Group", attribute: "group_id", type: "groupLink", hideOnSmall: true },
+    { name: t("dbobjects.parent") || "Parent", attribute: "father_id", type: "objectLink", hideOnSmall: true },
+    { name: t("dbobjects.name") || "Name", attribute: "name", type: "string", hideOnSmall: false },
+    { name: t("dbobjects.description") || "Description", attribute: "description", type: "string", hideOnSmall: true },
+    { name: t("common.language") || "Language", attribute: "language", type: "languageView", hideOnSmall: true },
+  ];
+  return (
+    <ObjectSearch searchClassname={searchClassname} searchColumns={searchColumns} resultsColumns={resultsColumns} dark={dark} themeClass={themeClass} />
+  );
 }
