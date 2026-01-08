8a21c6c60b451fc9e168e9d6e468d398ff49a4aa
Roberto@Zephyrus <roberto.roccoangeloni@gmail.com>
2026-01-08 10:10:11 +0100

    refined google and github login buttons

--- fe/src/Login.js
+++ fe/src/Login.js
@@ -79,26 +79,34 @@ function Login() {
           <div className="col-md-4"></div>
           <div className="col-md-8">
             <button className="btn btn-primary">{t("common.login")}</button>
-            <div className="mt-3">
+            </div>
+        </div>
               { (app_cfg.enable_google_oauth === 'true' || app_cfg.enable_google_oauth === true || app_cfg.enable_google_oauth === '1') && (
-                <button type="button" className="btn btn-outline-danger me-2" onClick={() => {
-                  const startUrl = `${app_cfg.endpoint}/oauth/google/start`;
-                  window.location.href = startUrl;
-                }}>
-                  {t("auth.sign_in_with_google") || 'Sign in with Google'}
-                </button>
+                <div className="form-group row mt-3 m-auto">
+                  <div className="col-md-12 text-center">
+                      <button type="button" className="btn btn-outline-secondary" onClick={() => {
+                        const startUrl = `${app_cfg.endpoint}/oauth/google/start`;
+                        window.location.href = startUrl;
+                      }}>
+                        <svg className="me-2" style={{height: '1.5rem'}} xmlns="https://www.w3.org/2000/svg" viewBox="0 0 48 48"><path fill="#4285F4" d="M45.12 24.5c0-1.56-.14-3.06-.4-4.5H24v8.51h11.84c-.51 2.75-2.06 5.08-4.39 6.64v5.52h7.11c4.16-3.83 6.56-9.47 6.56-16.17z"></path><path fill="#34A853" d="M24 46c5.94 0 10.92-1.97 14.56-5.33l-7.11-5.52c-1.97 1.32-4.49 2.1-7.45 2.1-5.73 0-10.58-3.87-12.31-9.07H4.34v5.7C7.96 41.07 15.4 46 24 46z"></path><path fill="#FBBC05" d="M11.69 28.18C11.25 26.86 11 25.45 11 24s.25-2.86.69-4.18v-5.7H4.34C2.85 17.09 2 20.45 2 24c0 3.55.85 6.91 2.34 9.88l7.35-5.7z"></path><path fill="#EA4335" d="M24 10.75c3.23 0 6.13 1.11 8.41 3.29l6.31-6.31C34.91 4.18 29.93 2 24 2 15.4 2 7.96 6.93 4.34 14.12l7.35 5.7c1.73-5.2 6.58-9.07 12.31-9.07z"></path><path fill="none" d="M2 2h44v44H2z"></path></svg>
+                        {t("auth.sign_in_with_google") || 'Sign in with Google'}
+                      </button>
+                  </div>
+                </div>
               ) }
               { (app_cfg.enable_github_oauth === 'true' || app_cfg.enable_github_oauth === true || app_cfg.enable_github_oauth === '1') && (
-                <button type="button" className="btn btn-outline-primary" onClick={() => {
-                  const startUrl = `${app_cfg.endpoint}/oauth/github/start`;
-                  window.location.href = startUrl;
-                }}>
-                  {t("auth.sign_in_with_github") || 'Sign in with GitHub'}
-                </button>
+                <div className="form-group row mt-3 m-auto">
+                  <div className="col-md-12 text-center">
+                      <button type="button" className="btn btn-outline-secondary" onClick={() => {
+                          const startUrl = `${app_cfg.endpoint}/oauth/github/start`;
+                          window.location.href = startUrl;
+                        }}>
+                        <svg className="me-2" xmlns="https://www.w3.org/2000/svg" viewBox="0 0 22 22" style={{height: '1.5rem'}}><path d="M12 1C5.923 1 1 5.923 1 12c0 4.867 3.149 8.979 7.521 10.436.55.096.756-.233.756-.522 0-.262-.013-1.128-.013-2.049-2.764.509-3.479-.674-3.699-1.292-.124-.317-.66-1.293-1.127-1.554-.385-.207-.936-.715-.014-.729.866-.014 1.485.797 1.691 1.128.99 1.663 2.571 1.196 3.204.907.096-.715.385-1.196.701-1.471-2.448-.275-5.005-1.224-5.005-5.432 0-1.196.426-2.186 1.128-2.956-.111-.275-.496-1.402.11-2.915 0 0 .921-.288 3.024 1.128a10.193 10.193 0 0 1 2.75-.371c.936 0 1.871.123 2.75.371 2.104-1.43 3.025-1.128 3.025-1.128.605 1.513.221 2.64.111 2.915.701.77 1.127 1.747 1.127 2.956 0 4.222-2.571 5.157-5.019 5.432.399.344.743 1.004.743 2.035 0 1.471-.014 2.654-.014 3.025 0 .289.206.632.756.522C19.851 20.979 23 16.854 23 12c0-6.077-4.922-11-11-11Z"></path></svg>
+                        {t("auth.sign_in_with_github") || 'Sign in with GitHub'}
+                      </button>
+                  </div>
+                </div>
               ) }
-            </div>
-          </div>
-        </div>
       </form>
     </div>
   );
